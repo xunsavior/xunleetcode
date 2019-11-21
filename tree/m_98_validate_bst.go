a6f9434e7ed47bfa60ef8b1018daa7bc9c670819
Xun Hu <xunhu19911223@gmail.com>
2019-11-21 21:41:16 +1100

    Update 98 is valid bst
    using a better recursion to solve the problem

--- tree/m_98_validate_bst.go
+++ tree/m_98_validate_bst.go
@@ -9,59 +9,23 @@ package tree
 	Both the left and right subtrees must also be binary search trees.
 */
 
-// IsValidBST ...
+// IsValidBST ... Solution 1: limit the value range for the sub-trees
 func IsValidBST(root *TNode) bool {
-	if root == nil {
-		return true
-	}
-	return checkIfIsValid(nil, nil, root, nil)
+	return checkIfIsValid(root, nil, nil)
 }
 
-func checkIfIsValid(min, max *int, currentNode *TNode, parentNode *TNode) bool {
+func checkIfIsValid(currentNode *TNode, min, max *int) bool {
 	// when we reach the leaf node
 	if currentNode == nil {
 		return true
 	}
-	// we must check if the value of current node is in (min, max)
-	if (min != nil && currentNode.Val <= *min) || (max != nil && currentNode.Val >= *max) {
+	// if the value of current node is NOT int the range of (min, max), return false
+	if (max != nil && currentNode.Val >= *max) ||
+		(min != nil && currentNode.Val <= *min) {
 		return false
 	}
 
-	isLeftValid, isRightValid := true, true
-	lNode, rNode := currentNode.Left, currentNode.Right
-	// check the left part
-	if lNode != nil {
-		if parentNode != nil {
-			/*
-				important:
-				not only we need to check if left node is less than current node,
-				we should also pay attention to the entire tree. (Same as the right side)
-				e.g. [3,1,5,0,2,4,6,null,null,null,3] and [3,null,30,10,null,null,15,null,45] are invalid
-
-				That is why we need both parent and current node to set up the max and min
-				value for the next child node.
-			*/
-			if parentNode.Right == currentNode {
-				isLeftValid = checkIfIsValid(&(parentNode.Val), &(currentNode.Val), lNode, currentNode)
-			} else {
-				isLeftValid = checkIfIsValid(min, &(currentNode.Val), lNode, currentNode)
-			}
-		} else {
-			isLeftValid = checkIfIsValid(min, &(currentNode.Val), lNode, currentNode)
-		}
-	}
-	//check the right part
-	if rNode != nil {
-		if parentNode != nil {
-			if parentNode.Left == currentNode {
-				isRightValid = checkIfIsValid(&(currentNode.Val), &(parentNode.Val), rNode, currentNode)
-			} else {
-				isRightValid = checkIfIsValid(&(currentNode.Val), max, rNode, currentNode)
-			}
-		} else {
-			isRightValid = checkIfIsValid(&(currentNode.Val), max, rNode, currentNode)
-		}
-	}
-	// we need validate if left and right side are both valid
-	return isRightValid && isLeftValid
+	return checkIfIsValid(currentNode.Left, min, &(currentNode.Val)) && checkIfIsValid(currentNode.Right, &(currentNode.Val), max)
 }
+
+// Solution 2: using inorder traversel and check if it is sorted or not
