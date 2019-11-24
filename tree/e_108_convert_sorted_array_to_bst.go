9725c70d64044130b27524a1846d76c445b277f3
Xun Hu <xunhu19911223@gmail.com>
2019-11-24 23:09:17 +1100

    108 is finished
    https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/

---
+++ tree/e_108_convert_sorted_array_to_bst.go
@@ -0,0 +1,48 @@
+package tree
+
+func sortedArrayToBST108(nums []int) *TNode {
+	size := len(nums)
+	if size == 0 {
+		return nil
+	}
+	return helper108(0, size-1, nums)
+}
+
+func helper108(start, end int, nums []int) *TNode {
+	// base case: the number of nodes of subtree is 2
+	if end-start == 1 {
+		return &TNode{
+			Val: nums[end],
+			Left: &TNode{
+				Val:   nums[start],
+				Left:  nil,
+				Right: nil,
+			},
+			Right: nil,
+		}
+	}
+
+	// base case: the number of nodes of subtree is 1
+	if end == start {
+		return &TNode{
+			Val:   nums[start],
+			Left:  nil,
+			Right: nil,
+		}
+	}
+
+	// we split the tree into left and right subtree using mid index (i.e. (start+end)/2 )
+	m := (end + start) / 2
+	node := &TNode{
+		Val:   nums[m],
+		Left:  nil,
+		Right: nil,
+	}
+
+	//get all left subtrees
+	node.Left = helper108(start, m-1, nums)
+	//get all right subtrees
+	node.Right = helper108(m+1, end, nums)
+
+	return node
+}
