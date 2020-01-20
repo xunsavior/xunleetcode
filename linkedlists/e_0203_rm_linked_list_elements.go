b37bb6f997440cf2379e1a1b05df61807024255b
Xun Hu <xunhu19911223@gmail.com>
2020-01-20 23:52:26 +1100

    updated 203

--- linkedlists/e_0203_rm_linked_list_elements.go
+++ linkedlists/e_0203_rm_linked_list_elements.go
@@ -10,9 +10,6 @@ Output: 1->2->3->4->5
 */
 
 func removeElements203(head *ListNode, val int) *ListNode {
-	if head == nil {
-		return nil
-	}
 	var last *ListNode
 	tmp := head
 	for tmp != nil {
@@ -20,12 +17,11 @@ func removeElements203(head *ListNode, val int) *ListNode {
 			if last != nil {
 				last.Next, tmp = tmp.Next, tmp.Next
 			} else {
-				head, tmp = head.Next, head
+				head, tmp = head.Next, head.Next
 			}
-		} else {
-			last, tmp = tmp, tmp.Next
+			continue
 		}
+		last, tmp = tmp, tmp.Next
 	}
-
 	return head
 }
